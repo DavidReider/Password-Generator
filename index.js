ed2971ac43ab0db7258b3c278253146f699684d3
DavidReider <freakdj1997@yahoo.com>
2022-04-10 20:53:35 -0400

    Plan for JavaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+(function () {
+  console.log("Init");
+
+  // AddEventListener to Button - Click
+  // When clicked, the button will call the function "generatePasswords"
+
+  function generatePasswords() {
+    //get configs if applicable
+    //get passwords
+  }
+
+  function getConfigs() {
+    //get configs from select fields
+  }
+
+  function insertPasswords() {
+    //insert passwords into text fields
+  }
+})();
