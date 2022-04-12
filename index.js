52df2041e9f496b86897337339cc2f78ffa451b5
DavidReider <freakdj1997@yahoo.com>
2022-04-11 21:05:47 -0400

    Collect config options in object

--- index.js
+++ index.js
@@ -3,14 +3,25 @@
 
   // AddEventListener to Button - Click
   // When clicked, the button will call the function "generatePasswords"
+  document
+    .getElementById("generate")
+    .addEventListener("click", generatePasswords);
 
   function generatePasswords() {
-    //get configs if applicable
+    console.log("generating...");
+    const configs = getConfigs();
+    console.log(configs);
     //get passwords
   }
 
   function getConfigs() {
     //get configs from select fields
+    let configs = {};
+    configs.length = document.getElementById("length").value;
+    configs.symbols = document.getElementById("symbols").checked;
+    configs.uppercase = document.getElementById("uppercase").checked;
+    configs.lowercase = document.getElementById("lowercase").checked;
+    return configs;
   }
 
   function insertPasswords() {
